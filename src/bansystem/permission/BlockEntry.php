b3755f7c815ab37c63003833efd0b69c765e3ac3
TheFixerDevelopment <30442910+TheFixerDevelopment@users.noreply.github.com>
2017-09-28 09:40:56 +0100

    Update BlockEntry.php

--- src/bansystem/permission/BlockEntry.php
+++ src/bansystem/permission/BlockEntry.php
@@ -6,7 +6,7 @@ use pocketmine\permission\BanEntry;
 
 class BlockEntry extends BanEntry {
     
-    public function __construct($name) {
+    public function __construct(string $name) {
         parent::__construct($name);
         $this->setReason("Blocked by an operator.");
     }
